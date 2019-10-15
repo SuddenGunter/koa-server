eb952c61a5ae8e7f77f95d5d9a67681e2c0c8409
Artem <kolomytsev1996@gmail.com>
2019-10-15 17:58:16 +0300

    add standardjs linter

--- app.js
+++ app.js
@@ -1,50 +1,48 @@
 'use strict'
 
-const Koa = require('koa');
-const gracefulShutdown = require('http-graceful-shutdown');
-const bodyParser = require('koa-bodyparser');
+const Koa = require('koa')
+const gracefulShutdown = require('http-graceful-shutdown')
+const bodyParser = require('koa-bodyparser')
 const json = require('koa-json')
 const pino = require('koa-pino-logger')
-const helmet = require("koa-helmet");
-const respond = require('koa-respond');
+const helmet = require('koa-helmet')
+const respond = require('koa-respond')
 const uuid = require('uuid')
-const app = new Koa();
+const app = new Koa()
 
 app.use(pino())
 app.silent = true
-app.use(helmet());
-app.use(respond());
-app.use(json({ pretty: app.env == "development" }))
-app.use(bodyParser());
+app.use(helmet())
+app.use(respond())
+app.use(json({ pretty: app.env === 'development' }))
+app.use(bodyParser())
 
 app.use(async (ctx, next) => {
   ctx.set('X-Request-ID', uuid.v4())
-  await next();
-});
+  await next()
+})
 
 app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-});
-
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  ctx.set('X-Response-Time', `${ms}ms`)
+})
 
 app.use((ctx) => {
   ctx.body = { foo: 'bar' }
 })
 
-
-var server = app.listen(3002);
+var server = app.listen(3002)
 
 // this enables the graceful shutdown with advanced options
 gracefulShutdown(server,
-	{
-		signals: 'SIGINT SIGTERM',
-		timeout: 10000,
-		development: false,
-		finally: function() {
-			console.log('Server shutted down')
-		}
-	}
-);
+  {
+    signals: 'SIGINT SIGTERM',
+    timeout: 10000,
+    development: false,
+    finally: () => {
+      console.log('Server shutted down')
+    }
+  }
+)
